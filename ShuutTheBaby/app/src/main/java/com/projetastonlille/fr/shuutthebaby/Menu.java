5f368fe2c84e824451a736046ae6a542003412da
Pierre Florquin <pierre.florquin@gmail.com>
2016-05-31 16:16:34 +0200

    Initial Commit to Develop
    New project

---
+++ ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Menu.java
@@ -0,0 +1,35 @@
+package com.projetastonlille.fr.shuutthebaby;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button buttonPlay = (Button) findViewById(R.id.playButton);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,Play.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonRec = (Button) findViewById(R.id.recButton);
+        buttonRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,Record.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Record.java
@@ -0,0 +1,67 @@
+package com.projetastonlille.fr.shuutthebaby;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+/**
+ * Created by Quang on 30/05/2016.
+ */
+public class Record extends Menu {
+    MediaRecorder recorder;
+    MediaPlayer player;
+    String LOG_TAG = "enregistrer";
+    private String fileRecord = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.record);
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(fileRecord);
+    }
+
+    public void startRecording(View view) {
+        try {
+            recorder.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+
+
+
+        Toast.makeText(this, "Recording", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopRecording(View view) {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+
+
+        Toast.makeText(Record.this, "End of the record", Toast.LENGTH_SHORT).show();
+    }
+
+    public void startPlaying(View view) throws IOException {
+        MediaPlayer recSound = new MediaPlayer();
+        recSound.setDataSource(fileRecord);
+        recSound.prepare();
+        recSound.start();
+
+        Toast.makeText(Record.this, "Playing Audio", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopPlaying(View view){
+        player.stop();
+    }
+}
