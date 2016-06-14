5e3f9fe1a8488549384026c405e85e7a78ee5eae
revoLille <pierre.florquin@gmail.com>
2016-06-14 13:58:03 +0200

    hinted register buttons
    hinted register buttons

--- ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Register.java
+++ ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Register.java
@@ -2,16 +2,16 @@ package com.projetastonlille.fr.shuutthebaby;
 
 import android.os.Bundle;
 
-
 /**
  * Created by Quang on 30/05/2016.
  */
-public class Register extends Menu{
+public class Register extends Menu {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
     }
 
 }
