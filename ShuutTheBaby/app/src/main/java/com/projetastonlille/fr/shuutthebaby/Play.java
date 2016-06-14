fe3c996e3525b8b437b4e0cb8755292d7b085b69
Aquang <dam.anhquang@gmail.com>
2016-06-14 17:03:12 +0200

    selectionmusique

--- ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Play.java
+++ ShuutTheBaby/app/src/main/java/com/projetastonlille/fr/shuutthebaby/Play.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 
@@ -20,39 +22,55 @@ public class Play extends Menu{
     MediaPlayer mySound;
     int paused;
     //Declaration des listes
-    private Map<String, Integer> soundMap = new HashMap<>();
+    private Map<String, Integer> _soundMap = new HashMap<>();
+    private Map<String, Integer> _timeMap = new HashMap<>();
 //    private Map<String, Integer> timeMap; // Liste en bonus avec Timer ;)
-
+    private Timer _timer = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play);
 
-        soundMap.put("L'enfant do", R.raw.enfantdo);
-        soundMap.put("Machine à laver", R.raw.dryer );
-        soundMap.put("Frere Jacques",R.raw.jacques);
-        soundMap.put("Meunier",R.raw.meunier);
+        _soundMap.put("L'enfant do", R.raw.enfantdo);
+        _soundMap.put("Machine à laver", R.raw.dryer );
+        _soundMap.put("Frere Jacques",R.raw.jacques);
+        _soundMap.put("Meunier",R.raw.meunier);
+
+        _timeMap.put(getResources().getString(R.string.disabled), 0);
+        _timeMap.put(getResources().getString(R.string.time_1minute), 1000*60*1);
+        _timeMap.put(getResources().getString(R.string.time_5minute), 1000*60*5);
+        _timeMap.put(getResources().getString(R.string.time_10minute), 1000*60*10);
+        _timeMap.put(getResources().getString(R.string.time_30minute), 1000*60*30);
+        _timeMap.put(getResources().getString(R.string.time_1hour), 1000*60*60*1);
+        _timeMap.put(getResources().getString(R.string.time_2hour), 1000*60*60*2);
+        _timeMap.put(getResources().getString(R.string.time_4hour), 1000*60*60*4);
+        _timeMap.put(getResources().getString(R.string.time_8hour), 1000*60*60*8);
+
+        final Spinner timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        List<String> times = new ArrayList<>(_timeMap.keySet());
+        ArrayAdapter<String> timesAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, times);
+        timesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timesAdapter);
+
 
         final Spinner soundSpinner = (Spinner) findViewById(R.id.soundSpinner);
-
-        List<String> names = new ArrayList<>(soundMap.keySet());
+        List<String> names = new ArrayList<>(_soundMap.keySet());
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,names);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         soundSpinner.setAdapter(dataAdapter);
-
-
 
     }
 
     public void playMusic(View view) {
         final Spinner soundSpinner = (Spinner) findViewById(R.id.soundSpinner);
         String selectedSound = (String)soundSpinner.getSelectedItem();
+        int id  = _soundMap.get(selectedSound);
 
-        int id = soundMap.get(selectedSound);
 
         if(mySound==null){
-            mySound = MediaPlayer.create(this, id);
+
+            mySound = MediaPlayer.create(this,id);
             mySound.setLooping(true);//Boucle de la musique a coupler avec un Timer ?
             mySound.start();
 
@@ -63,6 +81,8 @@ public class Play extends Menu{
     }
     public void stopMusic(View view) {
         mySound.pause();
+        mySound.release();
+        mySound = null;
         paused = 0;
     }
     //METHOD() Pause à linstant INT et on recupere cette valer avec le .getCurrentPosition.
